6c34e5c1abfd7b769a96bd6a86a2d88df13b13fb
dove <ndoverte11@gmail.com>
2023-08-10 08:56:16 +0800

    added js folder

---
+++ js/app.js
@@ -0,0 +1,5 @@
+import covidArr from './data.js'
+
+console.log(covidArr)
+
+console.log('hello')
---
+++ js/data.js
@@ -0,0 +1,24 @@
+const covidArr = [
+  {
+    'Active Cases_text': '+163,901',
+    Country_text: 'World',
+    'Last Update': '2022-10-02 06:52',
+    'New Cases_text': '+68,884',
+    'New Deaths_text': '+108',
+    'Total Cases_text': '623,268,353',
+    'Total Deaths_text': '6,549,980',
+    'Total Recovered_text': '603,127,630',
+  },
+  {
+    'Active Cases_text': '',
+    Country_text: 'USA',
+    'Last Update': '2022-10-02 06:52',
+    'New Cases_text': '',
+    'New Deaths_text': '',
+    'Total Cases_text': '98,248,623',
+    'Total Deaths_text': '1,084,891',
+    'Total Recovered_text': '95,113,327',
+  },
+]
+
+export default covidArr
